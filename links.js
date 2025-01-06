5c79b49044d203b78e9d8e93f502e2993c9c1a13
APIDOWNLOAD <A.P.IDOWNLOAD24@gmail.com>
2025-01-06 11:02:23 -0500

    commit

---
+++ links.js
@@ -0,0 +1,24 @@
+//LINK WISE MEMORY 
+function wmolink(){
+    window.open("https://www.mediafire.com/file/8mfkxwpll8lgs83/WMOSetup_4.2.2.128.exe/file","_blank");
+}
+//LINK ACTIVADOR WIN
+function actlink(){
+    window.open("https://www.mediafire.com/file/grj18sse7uro5wq/Activador_todos_Windows_y_office_Act_v4.0.0.rar/file","_blank");
+}
+//LINK OPTIMIZER 
+function optlink(){
+    window.open("https://www.mediafire.com/file/14fbkq439ur71mj/Optimizer-16.5.exe/file","_blank");
+}
+//LINK WINRAR
+function winrlink(){
+    window.open("https://www.mediafire.com/file/8ysa5sfrymox4nb/WinRAR-x64.exe/file#","_blank");
+}
+//LINK ABOBE RIDER
+function adblink(){
+    window.open("https://www.mediafire.com/file/swq8m0tfy487q7z/Adobe_Rider.rar/file","_blank");
+}
+//LINK LIBRE OFFICE
+function lbrlink(){
+    window.open("https://www.mediafire.com/file/t71ryxayv1lr25g/LibreOffice_24.8.4_Win_x86-64_helppack_es.msi/file","_blank");
+}
