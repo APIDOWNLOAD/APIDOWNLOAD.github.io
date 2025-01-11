381d2fc48e64de5abe65556ba96b5fa719f321ab
APIDOWNLOAD <A.P.IDOWNLOAD24@gmail.com>
2025-01-11 18:41:00 -0500

    commit

--- links.js
+++ links.js
@@ -1,7 +1,7 @@
 //LINK WISE MEMORY 
 function wmolink(){
     window.open("https://www.mediafire.com/file/8mfkxwpll8lgs83/WMOSetup_4.2.2.128.exe/file","_blank");
-    //alert("REDIRIGIENDO A MEDIAFIRE")
+    console.log("hola");
 }
 //LINK ACTIVADOR WIN
 function actlink(){
@@ -15,6 +15,10 @@ function optlink(){
 function winrlink(){
     window.open("https://www.mediafire.com/file/8ysa5sfrymox4nb/WinRAR-x64.exe/file#","_blank");
 }
+//LINK RUFUS
+function rufuslink(){
+    window.open("https://www.mediafire.com/file/0rm1d4etg4qcu9n/rufus-3.20.rar/file#");
+}
 //LINK ABOBE RIDER
 function adblink(){
     window.open("https://www.mediafire.com/file/swq8m0tfy487q7z/Adobe_Rider.rar/file","_blank");
@@ -22,4 +26,8 @@ function adblink(){
 //LINK LIBRE OFFICE
 function lbrlink(){
     window.open("https://www.mediafire.com/file/t71ryxayv1lr25g/LibreOffice_24.8.4_Win_x86-64_helppack_es.msi/file","_blank");
+}
+//LINK SUMATRA PDF
+function smtlink(){
+    window.open("https://www.mediafire.com/file/5c9arzq1a526f1a/SumatraPDF-3.5.2-64-install.exe/file");
 }
